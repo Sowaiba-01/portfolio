cad2239671d3738298e78198076c61e18c9ed976
Sowaiba-01 <sowaibaarshad@gmail.com>
2026-07-20 13:06:38 +0500

    add speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
   metadataBase: new URL('https://sowaibaarshad.com'),
@@ -38,6 +39,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
